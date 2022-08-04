82a9c32a5143a2d5bb1a335e4618ede468695097
perezchris3599 <kweeziperez712@gmail.com>
2022-08-04 13:16:31 +0300

    json file

--- postscrape/postscrape/spiders/posts_spider.py
+++ postscrape/postscrape/spiders/posts_spider.py
@@ -4,13 +4,21 @@ class PostsSpider(scrapy.Spider):
     name = "posts"
 
     start_urls = [
-        'https://blog.scrapinghub.com/page/1/',
-        'https://blog.scrapinghub.com/page/2'
+        'https://www.zyte.com/blog/'
     ]
 
     def parse(self, response):
-        page = response.url.split('/')[-1]
-        filename = 'posts-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-            
+        for post in response.css('div.post-item'):
+            yield {
+                'title': post.css('.post-header h2 a::text')[0].get(),
+                'date': post.css('.post-header a::text')[1].get(),
+                'author': post.css('.post-header a::text')[2].get()
+            }
+
+
+
+
+        #page = response.url.split('/')[-1]
+        #filename = 'posts-%s.html' % page
+        #with open(filename, 'wb') as f:
+            #f.write(response.body)
